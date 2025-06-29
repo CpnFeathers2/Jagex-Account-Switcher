c981198a1709a74416a7c9819588d6ccf10d74a2
FireStarter-Instigator <davidgatward27@gmail.com>
2025-06-29 02:43:33 +0200

    Update MainWindow.axaml.cs

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -2,6 +2,7 @@ using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Input;
 using Avalonia.Platform;
+using Avalonia.Interactivity;
 using JagexAccountSwitcher.ViewModels;
 
 namespace JagexAccountSwitcher;
